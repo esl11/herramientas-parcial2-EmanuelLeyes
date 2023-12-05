ecdef735f5ef3ee9e945591fd73427d398769bff
esl11 <esleyes@hotmail.com>
2023-12-05 18:48:52 -0300

    Modificaciones de leyendas

--- ViewModels/MovementCreateVM.cs
+++ ViewModels/MovementCreateVM.cs
@@ -4,8 +4,13 @@ namespace parcial2.ViewModels;
 
 public class MovementCreateVM
 {
+     [Display(Name = "Nro de Factura")]
     public int InvoiceNumber { get; set; }
+    
+     [Display(Name = "Fecha")]
     public DateTime Date { get; set; }
+
+     [Display(Name = "Cantidad")]
     public int Quantity { get; set; }
     public int FootwearId { get; set; }
 }
